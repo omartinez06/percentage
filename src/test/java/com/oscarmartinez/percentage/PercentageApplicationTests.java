06046340bebeb995553147523202a7b9579c8fd3
omartinez06 <omartinezordonez@gmail.com>
2025-05-05 20:43:46 -0600

    Add files via upload

---
+++ src/test/java/com/oscarmartinez/percentage/PercentageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarmartinez.percentage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PercentageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
